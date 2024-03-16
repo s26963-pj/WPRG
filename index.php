712db24027d0ee586012c9a06047ff605da87feb
s26963 <s26963@pjwstk.edu.pl>
2024-03-16 14:14:57 +0100

    First exercise done

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+
 <?php
-    echo "Hello PHP!";
+    //Zad 1
+    $tab = array("jablko", "banan", "pomarancza");
+    foreach ($tab as $fruit){
+        echo "$fruit - ";
+        $y = strlen($fruit) - 1;
+        for($i = 0; $i < strlen($fruit); $i++){
+            echo $fruit[$y];
+            $y--;
+        }
+        echo "\n";
+    }
+
+    //Zad2
 ?>
