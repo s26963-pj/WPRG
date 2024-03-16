4c641923e7617413438d46341c7eb21974b7312f
s26963 <s26963@pjwstk.edu.pl>
2024-03-16 14:29:16 +0100

    Second exercise done

--- index.php
+++ index.php
@@ -13,4 +13,21 @@
     }
 
     //Zad2
+
+    function pierwsza($n){
+        if ($n < 2){
+            return false;
+        }
+        $x = sqrt($n);
+        for ($i = 2; $i <= $x; $i++){
+            if (($n % $i) == 0) return false;
+        }
+        return true;
+    }
+    $zakres = array(1,20);
+    for ($i = $zakres[0]; $i <= $zakres[1]; $i++){
+        if(pierwsza($i)){
+            echo "$i\n";
+        }
+    }
 ?>
