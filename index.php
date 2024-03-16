c611bbe83907f53ec68665b47e87cf5111de2693
s26963 <s26963@pjwstk.edu.pl>
2024-03-16 14:37:57 +0100

    Partial exercise 3

--- index.php
+++ index.php
@@ -29,4 +29,16 @@
             echo "$i\n";
         }
     }
+
+    //Zad3
+    $n = 3;
+    function fib($num){
+        if ($num == 0) return 0;
+        else if ($num == 1) return 1;
+        else{
+            return fib($num - 1) + fib($num - 2);
+        }
+    }
+
+    echo fib($n);
 ?>
